57d40f879d76b380b76e033cc6d9732ae5c9d67a
Harish <harishjapa000@gmail.com>
2025-02-26 21:22:36 +0530

    4th file

--- day 3/d.py
+++ day 3/d.py
@@ -1,6 +1,3 @@
-# Given a sentence, use a dictionary comprehension to count the frequency of each word in
-# the sentence.
-
 sentence = "the quick brown fox jumps over the lazy dog"
 l=sentence.split()
 # print(l)
